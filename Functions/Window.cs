b971ba48058494ee52d285f9cc020feda0d91737
Scott Zimmerman <bighead.0@gmail.com>
2025-07-30 15:39:06 -0400

    Finish replacing/cleanup of original method which seems to work same as the old version.

--- Functions/Window.cs
+++ Functions/Window.cs
@@ -12,12 +12,6 @@ namespace AutoBorderless
         [return: MarshalAs(UnmanagedType.Bool)]
         private static extern bool IsWindowVisible(IntPtr hWnd);
 
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern bool MoveWindow(IntPtr hWnd, int x, int y, int nWidth, int nHeight, bool bRepaint);
-
-        [DllImport("user32.dll")]
-        private static extern bool AdjustWindowRect(ref Rectangle lpRect, long dwStyle, bool bMenu);
-
         [DllImport("user32.dll")]
         private static extern long GetWindowLong(IntPtr hWnd, long nIndex);
 
@@ -38,8 +32,6 @@ namespace AutoBorderless
 
         // Define constants
         private const int GWL_STYLE      = -16;
-        private const int GWL_EXSTYLE    = -20;
-
         private const int WS_BORDER = 0x00800000;
         private const int WS_DLGFRAME = 0x00400000;
         private const int WS_CAPTION = WS_BORDER | WS_DLGFRAME;
@@ -47,11 +39,6 @@ namespace AutoBorderless
         private const int WS_THICKFRAME = 0x00040000;
         private const int WS_MINIMIZEBOX = 0x00020000;
         private const int WS_MAXIMIZEBOX = 0x00010000;
-
-        private const long WS_POPUP      = 0x80000000;
-        private const long WS_VISIBLE    = 0x10000000;
-        private const long WS_EX_TOPMOST = 0x00000008;
-
         private const int SWP_FRAMECHANGED = 0x0020;
         private const int SWP_SHOWWINDOW = 0x0040;
         private const int SWP_NOZORDER = 0x0004;
@@ -65,36 +52,15 @@ namespace AutoBorderless
                 return true;
             return false;
         }
-        /*
-        public static void SetBorderlessA(IntPtr hWnd)
-        {
-            // Get screen size (based on primary monitor)
-            int screenWidth  = SystemInformation.PrimaryMonitorSize.Width;
-            int screenHeight = SystemInformation.PrimaryMonitorSize.Height;
 
-            // Rectangle
-            Rectangle winRect = new Rectangle(0, 0, screenWidth, screenHeight);
-
-            // Move and resize window to cover the full screen
-            SetWindowLong(hWnd, GWL_STYLE, WS_POPUP | WS_VISIBLE);
-            AdjustWindowRect(ref winRect, GetWindowLong(hWnd, GWL_STYLE), false);
-            SetWindowLong(hWnd, GWL_EXSTYLE, (GetWindowLong(hWnd, GWL_EXSTYLE) | WS_EX_TOPMOST));
-            MoveWindow(hWnd, winRect.Left, winRect.Top, winRect.Right - winRect.Left, winRect.Bottom - winRect.Top, true);
-        }
-        */
         public static void SetBorderless(IntPtr hWnd)
         {
-            long style = GetWindowLong(hWnd, GWL_STYLE);
-            style &= ~(WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX);
+            long style = GetWindowLong(hWnd, GWL_STYLE) & ~(WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX);
             SetWindowLong(hWnd, GWL_STYLE, style);
-
-            // Get the screen the window is on
             Screen screen = Screen.FromHandle(hWnd);
             var bounds = screen.Bounds;
-
-            SetWindowPos(hWnd, IntPtr.Zero, bounds.X, bounds.Y, bounds.Width, bounds.Height,
-                SWP_FRAMECHANGED | SWP_SHOWWINDOW | SWP_NOZORDER | SWP_NOOWNERZORDER | SWP_NOSENDCHANGING);
+            uint flags = SWP_FRAMECHANGED | SWP_SHOWWINDOW | SWP_NOZORDER | SWP_NOOWNERZORDER | SWP_NOSENDCHANGING;
+            SetWindowPos(hWnd, IntPtr.Zero, bounds.X, bounds.Y, bounds.Width, bounds.Height, flags);
         }
-
     }
 }
